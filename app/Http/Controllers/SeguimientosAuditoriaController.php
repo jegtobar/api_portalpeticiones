ce6d64257abde2a2eb9e3e057441329bdbaaf9ab
juanchemo <48888826+juanchemo@users.noreply.github.com>
2022-05-18 11:04:31 -0600

    Update

--- app/Http/Controllers/SeguimientosAuditoriaController.php
+++ app/Http/Controllers/SeguimientosAuditoriaController.php
@@ -7,7 +7,7 @@ class SeguimientosAuditoriaController extends Controller {
    
     public function reporteMntoSatisfechos(int $id){
         $reportes = DB::table('mnto_vecinos_satisfechos')
-        ->select('mnto_vecinos_satisfechos.id',  'personas.pNombre', 'personas.sNombre', 'personas.tNombre', 'personas.pApellido', 'personas.sApellido', 'personas.tApellido', DB::raw('DATE_FORMAT(mnto_vecinos_satisfechos.fecha,"%d/%m/%Y")AS fecha'), 'cat_actividades.actividad', 'mnto_vecinos_satisfechos.descripcion', 'mnto_vecinos_satisfechos.responsable','colonias.colonia','zonas.zona','distritos.distrito','sectores.sector','personas.created_at')
+        ->select('mnto_vecinos_satisfechos.id',  'personas.pNombre', 'personas.sNombre', 'personas.tNombre', 'personas.pApellido', 'personas.sApellido', 'personas.tApellido', DB::raw('DATE_FORMAT(mnto_vecinos_satisfechos.fecha,"%d/%m/%Y")AS fecha'), 'cat_actividades.actividad', 'mnto_vecinos_satisfechos.descripcion', 'mnto_vecinos_satisfechos.responsable','colonias.colonia','zonas.zona','distritos.distrito','sectores.sector','personas.created_at', 'personas.celular')
         ->leftJoin('personas','personas.id','=','mnto_vecinos_satisfechos.persona_id')
         ->leftJoin('cat_actividades','cat_actividades.id','=','mnto_vecinos_satisfechos.actividad_id')
         ->leftJoin('colonias','colonias.id','=','personas.colonia_id')
@@ -29,7 +29,7 @@ class SeguimientosAuditoriaController extends Controller {
 
     public function reporteMntoMuySatisfechos(int $id){
         $reportes = DB::table('mnto_vecinos_muysatisfechos')
-        ->select('mnto_vecinos_muysatisfechos.id',  'personas.pNombre', 'personas.sNombre', 'personas.tNombre', 'personas.pApellido', 'personas.sApellido', 'personas.tApellido', DB::raw('DATE_FORMAT(mnto_vecinos_muysatisfechos.fecha,"%d/%m/%Y")AS fecha'), 'cat_actividades.actividad', 'mnto_vecinos_muysatisfechos.descripcion', 'mnto_vecinos_muysatisfechos.responsable','colonias.colonia','zonas.zona','distritos.distrito','sectores.sector','personas.created_at')
+        ->select('mnto_vecinos_muysatisfechos.id',  'personas.pNombre', 'personas.sNombre', 'personas.tNombre', 'personas.pApellido', 'personas.sApellido', 'personas.tApellido', DB::raw('DATE_FORMAT(mnto_vecinos_muysatisfechos.fecha,"%d/%m/%Y")AS fecha'), 'cat_actividades.actividad', 'mnto_vecinos_muysatisfechos.descripcion', 'mnto_vecinos_muysatisfechos.responsable','colonias.colonia','zonas.zona','distritos.distrito','sectores.sector','personas.created_at','personas.celular')
         ->leftJoin('personas','personas.id','=','mnto_vecinos_muysatisfechos.persona_id')
         ->leftJoin('cat_actividades','cat_actividades.id','=','mnto_vecinos_muysatisfechos.actividad_id')
         ->leftJoin('colonias','colonias.id','=','personas.colonia_id')
--- app/Http/Controllers/SeguimientosExportController.php
+++ app/Http/Controllers/SeguimientosExportController.php
@@ -7,7 +7,7 @@ class SeguimientosExportController extends Controller {
    
     public function reporteSatisfechos(){
         $reportes = DB::table('snto_vecinos_satisfechos')
-        ->select('snto_vecinos_satisfechos.id',  'personas.pNombre', 'personas.sNombre', 'personas.tNombre', 'personas.pApellido', 'personas.sApellido', 'personas.tApellido', DB::raw('DATE_FORMAT(snto_vecinos_satisfechos.fecha,"%d/%m/%Y")AS fecha'), 'cat_actividades.actividad', 'snto_vecinos_satisfechos.descripcion', 'snto_vecinos_satisfechos.responsable','colonias.colonia','zonas.zona', 'distritos.distrito','sectores.sector','personas.created_at')
+        ->select('snto_vecinos_satisfechos.id',  'personas.pNombre', 'personas.sNombre', 'personas.tNombre', 'personas.pApellido', 'personas.sApellido', 'personas.tApellido', DB::raw('DATE_FORMAT(snto_vecinos_satisfechos.fecha,"%d/%m/%Y")AS fecha'), 'cat_actividades.actividad', 'snto_vecinos_satisfechos.descripcion', 'snto_vecinos_satisfechos.responsable','colonias.colonia','zonas.zona', 'distritos.distrito','sectores.sector','personas.created_at','personas.celular')
         ->leftJoin('personas','personas.id','=','snto_vecinos_satisfechos.persona_id')
         ->leftJoin('cat_actividades','cat_actividades.id','=','snto_vecinos_satisfechos.actividad_id')
         ->leftJoin('colonias','colonias.id','=','personas.colonia_id')
@@ -26,7 +26,7 @@ class SeguimientosExportController extends Controller {
 
     public function reportePersonasSinSeguimientoSatisfechos(){
         $reportes = DB::table('personas')
-        ->select('personas.id', 'personas.pNombre', 'personas.sNombre', 'personas.tNombre','personas.pApellido','personas.sApellido','personas.tApellido','personas.dpi',DB::raw('DATE_FORMAT(personas.nacimiento,"%d/%m/%Y")AS fecha'),'personas.nacimiento','personas.direccion','personas.numero_casa','zonas.zona','personas.colonia_id','colonias.colonia','colonias.distrito_id','personas.telefono_casa','personas.celular','personas.correo','personas.genero','liderazgos.liderazgo AS tipo','personas.liderazgo')
+        ->select('personas.id', 'personas.pNombre', 'personas.sNombre', 'personas.tNombre','personas.pApellido','personas.sApellido','personas.tApellido','personas.dpi',DB::raw('DATE_FORMAT(personas.nacimiento,"%d/%m/%Y")AS fecha'),'personas.nacimiento','personas.direccion','personas.numero_casa','zonas.zona','personas.colonia_id','colonias.colonia','colonias.distrito_id','personas.telefono_casa','personas.celular','personas.correo','personas.genero','liderazgos.liderazgo AS tipo','personas.liderazgo','personas.celular')
         ->join('zonas','zonas.id','=','personas.zona_id')
         ->join('liderazgos','liderazgos.id','=','personas.liderazgo')
         ->join('colonias','colonias.id','=','personas.colonia_id')
@@ -48,7 +48,7 @@ class SeguimientosExportController extends Controller {
 
     public function reporteMuySatisfechos(){
         $reportes = DB::table('snto_vecinos_muysatisfechos')
-        ->select('snto_vecinos_muysatisfechos.id',  'personas.pNombre', 'personas.sNombre', 'personas.tNombre', 'personas.pApellido', 'personas.sApellido', 'personas.tApellido', DB::raw('DATE_FORMAT(snto_vecinos_muysatisfechos.fecha,"%d/%m/%Y")AS fecha'), 'cat_actividades.actividad', 'snto_vecinos_muysatisfechos.descripcion', 'snto_vecinos_muysatisfechos.responsable','colonias.colonia','zonas.zona','distritos.distrito','sectores.sector','personas.created_at')
+        ->select('snto_vecinos_muysatisfechos.id',  'personas.pNombre', 'personas.sNombre', 'personas.tNombre', 'personas.pApellido', 'personas.sApellido', 'personas.tApellido', DB::raw('DATE_FORMAT(snto_vecinos_muysatisfechos.fecha,"%d/%m/%Y")AS fecha'), 'cat_actividades.actividad', 'snto_vecinos_muysatisfechos.descripcion', 'snto_vecinos_muysatisfechos.responsable','colonias.colonia','zonas.zona','distritos.distrito','sectores.sector','personas.created_at','personas.celular')
         ->leftJoin('personas','personas.id','=','snto_vecinos_muysatisfechos.persona_id')
         ->leftJoin('cat_actividades','cat_actividades.id','=','snto_vecinos_muysatisfechos.actividad_id')
         ->leftJoin('colonias','colonias.id','=','personas.colonia_id')
@@ -67,7 +67,7 @@ class SeguimientosExportController extends Controller {
 
     public function reportePersonasSinSeguimientoMuySatisfechos(){
         $reportes = DB::table('personas')
-        ->select('personas.id', 'personas.pNombre', 'personas.sNombre', 'personas.tNombre','personas.pApellido','personas.sApellido','personas.tApellido','personas.dpi',DB::raw('DATE_FORMAT(personas.nacimiento,"%d/%m/%Y")AS fecha'),'personas.nacimiento','personas.direccion','personas.numero_casa','zonas.zona','personas.colonia_id','colonias.colonia','colonias.distrito_id','personas.telefono_casa','personas.celular','personas.correo','personas.genero','liderazgos.liderazgo AS tipo','personas.liderazgo')
+        ->select('personas.id', 'personas.pNombre', 'personas.sNombre', 'personas.tNombre','personas.pApellido','personas.sApellido','personas.tApellido','personas.dpi',DB::raw('DATE_FORMAT(personas.nacimiento,"%d/%m/%Y")AS fecha'),'personas.nacimiento','personas.direccion','personas.numero_casa','zonas.zona','personas.colonia_id','colonias.colonia','colonias.distrito_id','personas.telefono_casa','personas.celular','personas.correo','personas.genero','liderazgos.liderazgo AS tipo','personas.liderazgo','personas.celular')
         ->join('zonas','zonas.id','=','personas.zona_id')
         ->join('liderazgos','liderazgos.id','=','personas.liderazgo')
         ->join('colonias','colonias.id','=','personas.colonia_id')
@@ -88,7 +88,7 @@ class SeguimientosExportController extends Controller {
 
     public function reporteMntoSatisfechos(){
         $reportes = DB::table('mnto_vecinos_satisfechos')
-        ->select('mnto_vecinos_satisfechos.id',  'personas.pNombre', 'personas.sNombre', 'personas.tNombre', 'personas.pApellido', 'personas.sApellido', 'personas.tApellido', DB::raw('DATE_FORMAT(mnto_vecinos_satisfechos.fecha,"%d/%m/%Y")AS fecha'), 'cat_actividades.actividad', 'mnto_vecinos_satisfechos.descripcion', 'mnto_vecinos_satisfechos.responsable','colonias.colonia','zonas.zona','distritos.distrito','sectores.sector','personas.created_at')
+        ->select('mnto_vecinos_satisfechos.id',  'personas.pNombre', 'personas.sNombre', 'personas.tNombre', 'personas.pApellido', 'personas.sApellido', 'personas.tApellido', DB::raw('DATE_FORMAT(mnto_vecinos_satisfechos.fecha,"%d/%m/%Y")AS fecha'), 'cat_actividades.actividad', 'mnto_vecinos_satisfechos.descripcion', 'mnto_vecinos_satisfechos.responsable','colonias.colonia','zonas.zona','distritos.distrito','sectores.sector','personas.created_at','personas.celular')
         ->leftJoin('personas','personas.id','=','mnto_vecinos_satisfechos.persona_id')
         ->leftJoin('cat_actividades','cat_actividades.id','=','mnto_vecinos_satisfechos.actividad_id')
         ->leftJoin('colonias','colonias.id','=','personas.colonia_id')
@@ -107,7 +107,7 @@ class SeguimientosExportController extends Controller {
 
     public function reportePersonasSinMntoSatisfechos(){
         $reportes = DB::table('personas')
-        ->select('personas.id', 'personas.pNombre', 'personas.sNombre', 'personas.tNombre','personas.pApellido','personas.sApellido','personas.tApellido','personas.dpi',DB::raw('DATE_FORMAT(personas.nacimiento,"%d/%m/%Y")AS fecha'),'personas.nacimiento','personas.direccion','personas.numero_casa','zonas.zona','personas.colonia_id','colonias.colonia','colonias.distrito_id','personas.telefono_casa','personas.celular','personas.correo','personas.genero','liderazgos.liderazgo AS tipo','personas.liderazgo')
+        ->select('personas.id', 'personas.pNombre', 'personas.sNombre', 'personas.tNombre','personas.pApellido','personas.sApellido','personas.tApellido','personas.dpi',DB::raw('DATE_FORMAT(personas.nacimiento,"%d/%m/%Y")AS fecha'),'personas.nacimiento','personas.direccion','personas.numero_casa','zonas.zona','personas.colonia_id','colonias.colonia','colonias.distrito_id','personas.telefono_casa','personas.celular','personas.correo','personas.genero','liderazgos.liderazgo AS tipo','personas.liderazgo','personas.celular')
         ->join('zonas','zonas.id','=','personas.zona_id')
         ->join('liderazgos','liderazgos.id','=','personas.liderazgo')
         ->join('colonias','colonias.id','=','personas.colonia_id')
@@ -128,7 +128,7 @@ class SeguimientosExportController extends Controller {
 
     public function reporteMntoMuySatisfechos(){
         $reportes = DB::table('mnto_vecinos_muysatisfechos')
-        ->select('mnto_vecinos_muysatisfechos.id',  'personas.pNombre', 'personas.sNombre', 'personas.tNombre', 'personas.pApellido', 'personas.sApellido', 'personas.tApellido', DB::raw('DATE_FORMAT(mnto_vecinos_muysatisfechos.fecha,"%d/%m/%Y")AS fecha'), 'cat_actividades.actividad', 'mnto_vecinos_muysatisfechos.descripcion', 'mnto_vecinos_muysatisfechos.responsable','colonias.colonia','zonas.zona','distritos.distrito','sectores.sector','personas.created_at')
+        ->select('mnto_vecinos_muysatisfechos.id',  'personas.pNombre', 'personas.sNombre', 'personas.tNombre', 'personas.pApellido', 'personas.sApellido', 'personas.tApellido', DB::raw('DATE_FORMAT(mnto_vecinos_muysatisfechos.fecha,"%d/%m/%Y")AS fecha'), 'cat_actividades.actividad', 'mnto_vecinos_muysatisfechos.descripcion', 'mnto_vecinos_muysatisfechos.responsable','colonias.colonia','zonas.zona','distritos.distrito','sectores.sector','personas.created_at','personas.celular')
         ->leftJoin('personas','personas.id','=','mnto_vecinos_muysatisfechos.persona_id')
         ->leftJoin('cat_actividades','cat_actividades.id','=','mnto_vecinos_muysatisfechos.actividad_id')
         ->leftJoin('colonias','colonias.id','=','personas.colonia_id')
@@ -147,7 +147,7 @@ class SeguimientosExportController extends Controller {
 
     public function reportePersonasSinMntoMuySatisfechos(){
         $reportes = DB::table('personas')
-        ->select('personas.id', 'personas.pNombre', 'personas.sNombre', 'personas.tNombre','personas.pApellido','personas.sApellido','personas.tApellido','personas.dpi',DB::raw('DATE_FORMAT(personas.nacimiento,"%d/%m/%Y")AS fecha'),'personas.nacimiento','personas.direccion','personas.numero_casa','zonas.zona','personas.colonia_id','colonias.colonia','colonias.distrito_id','personas.telefono_casa','personas.celular','personas.correo','personas.genero','liderazgos.liderazgo AS tipo','personas.liderazgo')
+        ->select('personas.id', 'personas.pNombre', 'personas.sNombre', 'personas.tNombre','personas.pApellido','personas.sApellido','personas.tApellido','personas.dpi',DB::raw('DATE_FORMAT(personas.nacimiento,"%d/%m/%Y")AS fecha'),'personas.nacimiento','personas.direccion','personas.numero_casa','zonas.zona','personas.colonia_id','colonias.colonia','colonias.distrito_id','personas.telefono_casa','personas.celular','personas.correo','personas.genero','liderazgos.liderazgo AS tipo','personas.liderazgo','personas.celular')
         ->join('zonas','zonas.id','=','personas.zona_id')
         ->join('liderazgos','liderazgos.id','=','personas.liderazgo')
         ->join('colonias','colonias.id','=','personas.colonia_id')
