7e8799d670beb68a24cc9de983246f423f81cbef
Juan García <48888826+juanchemo@users.noreply.github.com>
2022-07-12 19:36:37 -0600

    Add files via upload

--- app/Http/Controllers/SeguimientosController.php
+++ app/Http/Controllers/SeguimientosController.php
@@ -8,19 +8,19 @@ class SeguimientosController extends Controller
 {
     //Mostrar seguimientos  de vecinos segun id
     function getSeguimientosById(int $id){
-        $query = "SELECT a.id, a.persona_id, DATE_FORMAT(a.fecha,'%d/%m/%Y')AS date, a.fecha, a.descripcion, a.responsable, a.realizado, c.actividad, 1 AS mantenimiento FROM snto_vecinos_satisfechos a
+        $query = "SELECT a.id, a.persona_id, DATE_FORMAT(a.fecha,'%d/%m/%Y')AS date, a.fecha, a.actividad_id, a.descripcion, a.responsable, a.realizado, c.actividad, 1 AS mantenimiento FROM snto_vecinos_satisfechos a
         INNER JOIN cat_actividades c ON a.actividad_id = c.id
         WHERE a.persona_id = $id
         UNION
-        SELECT b.id, b.persona_id, DATE_FORMAT(b.fecha,'%d/%m/%Y')AS date, b.fecha, b.responsable, b.descripcion, b.realizado, c.actividad, 2 AS mantenimiento FROM mnto_vecinos_satisfechos b
+        SELECT b.id, b.persona_id, DATE_FORMAT(b.fecha,'%d/%m/%Y')AS date, b.fecha, b.actividad_id, b.descripcion, b.responsable, b.realizado, c.actividad, 2 AS mantenimiento FROM mnto_vecinos_satisfechos b
         INNER JOIN cat_actividades c ON b.actividad_id = c.id
         WHERE b.persona_id = $id
         UNION
-        SELECT a.id, a.persona_id, DATE_FORMAT(a.fecha,'%d/%m/%Y')AS date, a.fecha, a.responsable, a.descripcion, a.realizado, c.actividad, 3 AS mantenimiento FROM snto_vecinos_muysatisfechos a
+        SELECT a.id, a.persona_id, DATE_FORMAT(a.fecha,'%d/%m/%Y')AS date, a.fecha, a.actividad_id, a.descripcion, a.responsable, a.realizado, c.actividad, 3 AS mantenimiento FROM snto_vecinos_muysatisfechos a
         INNER JOIN cat_actividades c ON a.actividad_id = c.id
         WHERE a.persona_id = $id
         UNION
-        SELECT b.id, b.persona_id, DATE_FORMAT(b.fecha,'%d/%m/%Y')AS date, b.fecha, b.responsable, b.descripcion,  b.realizado, c.actividad, 4 AS mantenimiento FROM mnto_vecinos_muysatisfechos b
+        SELECT b.id, b.persona_id, DATE_FORMAT(b.fecha,'%d/%m/%Y')AS date, b.fecha, b.actividad_id, b.descripcion, b.responsable, b.realizado, c.actividad, 4 AS mantenimiento FROM mnto_vecinos_muysatisfechos b
         INNER JOIN cat_actividades c ON b.actividad_id = c.id
         WHERE b.persona_id = $id";
         $response = DB::select($query);
